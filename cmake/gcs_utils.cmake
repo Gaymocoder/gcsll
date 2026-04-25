4d0a26bd8d0a4e8e6299d785d66416013c18f697
Gaymocoder <gaymocoder@gmail.com>
2026-04-25 03:10:02 +0300

    B&W | Modified CMake gcs_utils
    — Removed feature with "gcsll_include_dir"-target;
    
    — Added global variable "GCS_INCLUDE_DIRS";
    — Added function "gcs_object_prepare", which:
        — Adds C++23 features (like std::print);
        — Adds GCS_INCLUDE_DIRS to target include directories;
    — Added function "gcs_export_prepare", which prepares target for installation export:
        — Adds to target sources objects from the passed list;
        — Makes target aliase and name for installation export:
            — gcsll_utils -> gcsll::utils
            ('gcsll' — prefix, 'utils' — module)
            — gcsll -> gcsll::gcsll
            ('gcsll' — prefix, 'gcsll' — module)
        — Adds C++23 features (like std::print);
        — Adds GCS_INCLUDE_DIRS to target include directories;
    
    — Builds and works properly.

--- cmake/gcs_utils.cmake
+++ cmake/gcs_utils.cmake
@@ -1,13 +1,37 @@
-function(gcs_init_include target_name dir_path)
-    if(TARGET "${target_name}")
-        return()
+function(gcs_export_prepare target_name)
+    message("gcs_utils: Exporting ${target_name}")
+
+    foreach(obj IN LISTS ARGN)
+        message("Adding source-object '${obj}' to target '${target_name}'")
+        target_sources(${target_name} PRIVATE $<TARGET_OBJECTS:${obj}>)
+    endforeach()
+
+    string(FIND "${target_name}" "_" POS)
+    if(POS EQUAL -1)
+        set(MODULE "${target_name}")
+        set(PREFIX "${target_name}")
+    else()
+        string(SUBSTRING "${target_name}" 0 ${POS} PREFIX)
+        math(EXPR RIGHT_START "${POS} + 1")
+        string(SUBSTRING "${target_name}" ${RIGHT_START} -1 MODULE)
     endif()
 
-    add_library("${target_name}" INTERFACE)
-    target_include_directories("${target_name}" INTERFACE
-        $<BUILD_INTERFACE:${dir_path}>
+    add_library("${PREFIX}::${MODULE}" ALIAS "${target_name}")
+    target_include_directories("${target_name}" PUBLIC
+        $<BUILD_INTERFACE:${GCS_INCLUDE_DIRS}>
         $<INSTALL_INTERFACE:include>
     )
+    set_target_properties("${target_name}" PROPERTIES EXPORT_NAME "${MODULE}")
+    target_compile_features("${target_name}" INTERFACE cxx_std_23)
+    if (MINGW)
+        target_link_libraries("${target_name}" INTERFACE stdc++exp)
+    endif()
+    
+    message("gcs_utils: Exported (with prefix = '${PREFIX}', module = '${MODULE}')")
+endfunction()
+
+function(gcs_object_prepare target_name)
+    target_include_directories("${target_name}" PUBLIC "${GCS_INCLUDE_DIRS}")
 
     target_compile_features("${target_name}" INTERFACE cxx_std_23)
     if (MINGW)
