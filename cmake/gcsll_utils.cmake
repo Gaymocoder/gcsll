a6a9ea6a5b7af5ca1bb75965b3fd5469b73df604
Gaymocoder <gaymocoder@gmail.com>
2026-09-10 13:42:19 +0300

    B&W | Reworked "gcsll_link_labs"-function
    — Per-compiler whole-archive flags were replaced with the "$<LINK_LIBRARY:WHOLE_ARCHIVE>"-genex.

--- cmake/gcsll_utils.cmake
+++ cmake/gcsll_utils.cmake
@@ -1,11 +1,5 @@
 function(gcsll_link_labs TARGET)
     foreach(LAB IN LISTS ARGN)
-        if(MSVC)
-            target_link_libraries(${TARGET} PRIVATE /WHOLEARCHIVE:${LAB})
-        else()
-            target_link_libraries(${TARGET} PRIVATE
-                -Wl,--whole-archive ${LAB} -Wl,--no-whole-archive
-            )
-        endif()
+        target_link_libraries(${TARGET} PRIVATE "$<LINK_LIBRARY:WHOLE_ARCHIVE,${LAB}>")
     endforeach()
 endfunction()
